12a34ad077484b8d3af7f8937065d20c60858c5d
damien <damien@localhost.localdomain>
2015-11-23 21:26:41 +0100

    damien: gere les commentaires

--- Syntese_vhdl/fct_lexem_endl.cpp
+++ Syntese_vhdl/fct_lexem_endl.cpp
@@ -43,13 +43,15 @@ void fct_lexem_endl(string nom_fichier_en_min)
                         {
                             Current_State=1;
                         } 
-                        else
-                        {
-                            if(caractere=='_')
+                        else if(caractere=='_')
                             {
                                 Current_State=7;
                             }
+                        else if(caractere==('-'))
+                        {
+                            Current_State=3;
                         }
+                        break;
                     break;
                     
                     case 1:         //caractère==lettre
@@ -70,13 +72,14 @@ void fct_lexem_endl(string nom_fichier_en_min)
                             Current_State=2;
                         }
                         
-                        else if(caractere==('+' or '-'))
+                        else if(caractere==('-'))
                         {
-                            Current_State=4;
+                            Current_State=3;
                         }
+                       
                         break;
                         
-                    case 2:
+                    case 2:     //caractère precedent == "_"
                         if((caractere >= 0x61 and caractere <= 0x7A) or (caractere >='0' and caractere <= '9'))
                         {
                             fichier_lexem_endl << caractere_preced;
@@ -88,11 +91,31 @@ void fct_lexem_endl(string nom_fichier_en_min)
                         }
                     break;
                     
-                    case 3:
+                    case 3: //caractère précédent == "-"
+                        if((caractere >= 0x61 and caractere <= 0x7A) or (caractere >='0' and caractere <= '9'))
+                        {
+                            fichier_lexem_endl << endl << caractere_preced << endl;
+                            Current_State=1;
+                        }
+                         else if (caractere==32)
+                        {                     
+                            fichier_lexem_endl<<caractere_preced << endl;
+                            Current_State=0;
+                        }
+                        else if(caractere=='-')
+                        {
+                            Current_State=4;
+                        }
                         
                     break;
                                 
-                                
+                    case 4: //caractères précédents == "--" commentaire
+                        if(caractere=='\n')
+                        {
+                            fichier_lexem_endl <<endl;
+                            Current_State=0;
+                        }
+                    break;            
                         
                     case 7:             //erreur
                         cout<< "Grosse erreur de porc" << endl<<caractere_preced <<caractere<<endl<<Current_State;
