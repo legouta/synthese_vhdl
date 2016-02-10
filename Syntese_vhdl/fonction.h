60c873c0168a88a4a37e55986b0ba7b69ae321d0
damien <damien@localhost.localdomain>
2016-02-10 14:30:35 +0100

    dams :

--- Syntese_vhdl/fonction.h
+++ Syntese_vhdl/fonction.h
@@ -44,13 +44,13 @@ list<string>::iterator while_conditionnel(list<string>::iterator i,tree<string> 
 void check(tree<string> &tr,list<string> &liste_nom,list<string> &liste_sous_type, list<string> &liste_type, list<string> &liste_taille);
 
 
-//void synthese(tree<string> &tr, string path_synthese, string path_signaux_interm,string path_portes_interm);
+void synthese(tree<string> &tr, string path_synthese, string path_signaux_interm,string path_portes_interm, list<string> &liste_nom, list<string> &liste_type);
 void recopie_fichier(string path_cible, string path_source);
 list<string>::iterator check_assignation(tree<string> &tr);
-void synt_affec(tree<string> &tr, tree<string>::iterator it, string path_synthese, string path_signaux_interm,string path_portes_interm, list<string> portes_util);
-void synth_source_bis(tree<string> &tr, tree<string>::iterator it, string path_signaux_interm,string path_portes_interm, list<string> &portes_util);
+void synt_affec(tree<string> &tr, tree<string>::iterator it, string path_synthese, string path_signaux_interm,string path_portes_interm, list<string> portes_util,list<string> &liste_nom,list<string> &liste_type);
+void synth_source_bis(tree<string> &tr, tree<string>::iterator it, string path_signaux_interm,string path_portes_interm, list<string> &portes_util,list<string> &liste_nom,list<string> &liste_type);
 string creer_sig_int(string path_signaux_interm);
-void synth_porte(string source_a,string operateur,string source_b, string destination, string path_portes_interm, list<string> portes_util, string path_signaux_interm);
+void synth_porte(string source_a,string operateur,string source_b, string destination, string path_portes_interm, list<string> portes_util, string path_signaux_interm,list<string> &liste_nom,list<string> &liste_type);
 
 
 void print_tree(const tree<string> &tr, tree<string>::pre_order_iterator it, tree<string>::pre_order_iterator end, string chemin_5);
